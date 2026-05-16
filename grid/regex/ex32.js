d651018b32af9a1088ad554c0604c059f87264ea
Skycader <62342371+Skycader@users.noreply.github.com>
2026-05-17 01:34:35 +0300

    master: ex32 fix!

--- grid/regex/ex32.js
+++ grid/regex/ex32.js
@@ -1,15 +1,10 @@
 /*
 
-- Ex 2
+- Ex 32
 - Extract only "clean" targets (wrapped in symbols like #, *, &)
 - and ignore targets containing any digits.
 - ★Сложность: ★★★☆☆☆☆☆☆ (3/10)  
 
 */
 
-const r = /(\p{P})([a-z]+)\1/gimu;
-const f = (text) => {
-  const matches = [...text.matchAll(r)];
-  return matches.map((m) => m[2]);
-};
 module.exports = f;
