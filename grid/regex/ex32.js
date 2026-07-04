acaafa14fbde3e14058c12219d7b110857466042
Skycader <62342371+Skycader@users.noreply.github.com>
2026-07-05 00:42:52 +0300

    OK

--- grid/regex/ex32.js
+++ grid/regex/ex32.js
@@ -2,8 +2,9 @@
 
 - Ex 32
 - Extract only "clean" targets (wrapped in symbols like #, *, &)
-- and ignore targets containing any digits.
-- #danger! *missile* @fake@ &target# ===> missle fake  
+- Support all possible punctuaiton symbols
+- Allow only letters -> Digits or any other symbol are PROHIBITED
+- #danger! *missile* @fake@ &target# #non_2_valid# ===> missle fake  
 */
 
 module.exports = f;
