d331265f90f9ec35dfc64f4abf09c901a727522e
Skycader <62342371+Skycader@users.noreply.github.com>
2026-07-08 11:42:17 +0300

    ex14 upd

--- grid/regex/ex14.js
+++ grid/regex/ex14.js
@@ -3,15 +3,59 @@
  * tag where href is the full URL and link text is the second-level domain.
  *
  * Examples:
- *   "Follow us on https://twitter.com/user/axl" → 'Follow us on <a href="https://twitter.com/user/axl">twitter</a>'
- *   "and https://instagram.com/user/axl" → 'and <a href="https://instagram.com/user/axl">instagram</a>'
- *   "Visit http://example.com" → 'Visit <a href="http://example.com">example</a>'
- *   "Go to https://www.instagram.com/user/axl" → 'Go to <a href="https://www.instagram.com/user/axl">instagram</a>'
- *   "go to https://sub.example.com/path" → 'go to <a href="https://sub.example.com/path">example</a>'
- *   "see https://example.com/some-page" → 'see <a href="https://example.com/some-page">example</a>'
- *   "hello world" → "hello world"
- *   "" → ""
  *
+ * [1]  "Follow us on https://twitter.com/user/axl"
+ *      → 'Follow us on <a href="https://twitter.com/user/axl">twitter</a>'
+ *
+ * [2]  "and https://instagram.com/user/axl"
+ *      → 'and <a href="https://instagram.com/user/axl">instagram</a>'
+ *
+ * [3]  "Visit http://example.com"
+ *      → 'Visit <a href="http://example.com">example</a>'
+ *
+ * [4]  "Go to https://www.instagram.com/user/axl"
+ *      → 'Go to <a href="https://www.instagram.com/user/axl">instagram</a>'
+ *
+ * [5]  "go to https://sub.example.com/path"
+ *      → 'go to <a href="https://sub.example.com/path">example</a>'
+ *
+ * [6]  "see https://example.com/some-page"
+ *      → 'see <a href="https://example.com/some-page">example</a>'
+ *
+ * [7]  "Check https://instagram.com"
+ *      → 'Check <a href="https://instagram.com">instagram</a>'
+ *
+ * [8]  "Visit https://twitter.com"
+ *      → 'Visit <a href="https://twitter.com">twitter</a>'
+ *
+ * [9]  "visit http://example.com/page"
+ *      → 'visit <a href="http://example.com/page">example</a>'
+ *
+ * [10] "Go to https://account.google.com"
+ *      → 'Go to <a href="https://account.google.com">google</a>'
+ *
+ * [11] "Visit https://my-site.com"
+ *      → 'Visit <a href="https://my-site.com">my-site</a>'
+ *
+ * [12] "hello world"
+ *      → "hello world"
+ *
+ * [13] ""
+ *      → ""
+ *
+ * Known limitations (still should pass):
+ *
+ * [14] "Search https://twitter.com/search?q=foo"
+ *      → 'Search <a href="https://twitter.com/search?q=foo">twitter</a>'
+ *
+ * [15] "See https://github.com/user/repo#readme"
+ *      → 'See <a href="https://github.com/user/repo#readme">github</a>'
+ *
+ * [16] "Docs at https://example.com/page.html"
+ *      → 'Docs at <a href="https://example.com/page.html">example</a>'
+ *
+ * [17] "https://sub.www.twitter.com/path"
+ *      → '<a href="https://sub.www.twitter.com/path">twitter</a>'
  */
 
 module.exports = f;
